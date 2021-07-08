bb4663b7d96e29f8395a5e0c18eef5aa60e225c6
Piotr <piotr.wiorek71@gmail.com>
2021-07-08 18:52:31 +0200

    refactor(routing): Change relativity of NavbarComponent in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'app', component: NavbarComponent, children: [
+
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
